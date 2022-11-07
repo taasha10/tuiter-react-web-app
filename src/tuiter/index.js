343cd77d4233add55addc35b9d420abafc25371a
taasha10 <sharmapratyasha1@gmail.com>
2022-11-06 23:29:23 -0500

    a7 Redux fa22

--- src/tuiter/index.js
+++ src/tuiter/index.js
@@ -4,16 +4,18 @@ import HomeComponent from "./home";
 import NavigationSidebar from "./navigation-sidebar";
 import WhoToFollowList from "./who-to-follow-list";
 import {Routes, Route} from "react-router";
+import profileReducer
+    from "./profile/profile-reducer";
+import tuitsReducer from "./tuits/tuits-reducer";
 import whoReducer
     from "./reducers/who-reducer";
-import tuitsReducer from "./tuits/tuits-reducer";
 import { configureStore }
     from '@reduxjs/toolkit';
 import {Provider} from "react-redux";
 import ProfileComponent from "./profile";
 import EditProfileComponent from "./edit-profile";
 const store = configureStore(
-    {reducer: {who: whoReducer, tuits: tuitsReducer}});
+    {reducer: {who: whoReducer, tuits: tuitsReducer, profile: profileReducer}});
 
 
 function Tuiter() {
